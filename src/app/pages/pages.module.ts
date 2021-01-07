c3527a3863d3738464e762841a55b0b6f9b95790
julius-2010 <ing_04_15@hotmail.com>
2021-01-07 01:31:33 -0500

    Modificaciones en los modulos

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+
+import { PAGES_ROUTES } from './pages.routes';
+import { sharedModule } from '../shared/shared.module';
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+@NgModule({
+    declarations: [
+        PagesComponent,
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    exports: [
+        DashboardComponent,
+        ProgressComponent,
+        Graficas1Component
+    ],
+    imports: [
+        sharedModule,
+        PAGES_ROUTES
+    ]
+})
+
+export class PagesModule { }
